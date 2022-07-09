47e5c73a81d0dc505b4d6deda5313d9d01cfe081
Danilo Pianini <danilo.pianini@gmail.com>
2022-07-09 14:20:43 +0200

    build: enable the git hooks plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,15 @@ enableFeaturePreview("VERSION_CATALOGS")
 
 plugins {
     id("com.gradle.enterprise") version "3.10.2"
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "1.0.14"
+}
+
+gitHooks {
+    preCommit {
+        tasks("ktlintCheck")
+    }
+    commitMsg { conventionalCommits() }
+    createHooks()
 }
 
 gradleEnterprise {
