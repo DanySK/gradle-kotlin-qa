5a50d58181fda064efd1e72ce93814d1f59e34e0
Danilo Pianini <danilo.pianini@unibo.it>
2022-07-31 12:56:17 +0200

    fix(cpd): raise the default token toleration for Kotlin to 70, as 40 generates many false warnings

--- src/main/kotlin/org/danilopianini/kotlinqa/KotlinQAPlugin.kt
+++ src/main/kotlin/org/danilopianini/kotlinqa/KotlinQAPlugin.kt
@@ -109,7 +109,7 @@ open class KotlinQAPlugin : Plugin<Project> {
 
     companion object {
         private const val VERSIONS = "org/danilopianini/kotlinqa/versions.properties"
-        private const val DEFAULT_CPD_TOKENS_FOR_KOTLIN = 40
+        private const val DEFAULT_CPD_TOKENS_FOR_KOTLIN = 70
         private fun Properties.forLibrary(key: String): String = checkNotNull(get(key)?.toString()) {
             "Unable to read the default version of $key"
         }
