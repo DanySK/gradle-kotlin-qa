6364768c382fe5c02a1cdf0a59cc6ccdb6b3f0f7
Danilo Pianini <danilo.pianini@gmail.com>
2023-02-19 20:05:01 +0100

    test: copy testkit-gradle.properties into the test repos

--- src/test/kotlin/org/danilopianini/kotlinqa/test/Tests.kt
+++ src/test/kotlin/org/danilopianini/kotlinqa/test/Tests.kt
@@ -17,6 +17,10 @@ import java.io.File
 
 class Tests : StringSpec(
     {
+        val testkitProperties = javaClass.classLoader.getResourceAsStream("testkit-gradle.properties")
+        checkNotNull(testkitProperties) {
+            "No file testkit-gradle.properties was generated"
+        }
         val scan = ClassGraph()
             .enableAllInfo()
             .acceptPackages(Tests::class.java.`package`.name)
@@ -47,6 +51,7 @@ class Tests : StringSpec(
                         properties.writeText(testkit)
                         log.debug("written $testkit to ${properties.absolutePath}")
                     }
+                    testkitProperties.copyTo(File(testFolder.root, "gradle.properties").outputStream())
                     val result = GradleRunner.create()
                         .withProjectDir(testFolder.root)
                         .withPluginClasspath()
