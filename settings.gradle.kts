6956f3a54d7472e499da8ca1375ec6ea86b2a888
Danilo Pianini <danilo.pianini@gmail.com>
2024-06-06 00:27:26 +0200

    build: switch to develocity

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "gradle-kotlin-qa"
 
 plugins {
-    id("com.gradle.enterprise") version "3.17.4"
+    id("com.gradle.develocity") version "3.17.4"
     id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.7"
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
@@ -14,10 +14,10 @@ gitHooks {
     createHooks()
 }
 
-gradleEnterprise {
+develocity {
     buildScan {
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        termsOfServiceAgree = "yes"
-        publishOnFailure()
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
+        uploadInBackground = !System.getenv("CI").toBoolean()
     }
 }
