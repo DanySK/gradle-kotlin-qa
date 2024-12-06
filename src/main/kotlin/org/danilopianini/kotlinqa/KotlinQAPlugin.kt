a82716c5a9aa80a510a66f78575272b34ad73bfc
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-12-06 12:35:13 +0100

    feat: enable by default SARIF reports for detekt and ktlint
    # Conflicts:
    #	src/main/kotlin/org/danilopianini/kotlinqa/KotlinQAPlugin.kt

--- src/main/kotlin/org/danilopianini/kotlinqa/KotlinQAPlugin.kt
+++ src/main/kotlin/org/danilopianini/kotlinqa/KotlinQAPlugin.kt
@@ -28,6 +28,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
 import org.jlleitschuh.gradle.ktlint.KtlintExtension
 import org.jlleitschuh.gradle.ktlint.KtlintPlugin
 import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+import org.jlleitschuh.gradle.ktlint.tasks.GenerateReportsTask
 
 /**
  * Entry point for the Kotlin QA Plugin.
