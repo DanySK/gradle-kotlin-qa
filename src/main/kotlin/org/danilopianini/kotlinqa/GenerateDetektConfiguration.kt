190ab9484e6087110a3c974b13e4ae9161e8064a
Danilo Pianini <danilo.pianini@unibo.it>
2025-07-29 17:06:41 +0200

    style: update the editorconfig (#1125)

--- src/main/kotlin/org/danilopianini/kotlinqa/GenerateDetektConfiguration.kt
+++ src/main/kotlin/org/danilopianini/kotlinqa/GenerateDetektConfiguration.kt
@@ -11,40 +11,33 @@ import org.gradle.api.tasks.TaskAction
  * A Gradle task that copies the default configuration from the classpath into a target file.
  */
 open class GenerateDetektConfiguration
-    @Inject
-    constructor(
-        private val extension: KotlinQAExtension,
-    ) : DefaultTask() {
-        /**
-         * The output file (read only, must be configured in the [KotlinQAExtension].
-         */
-        @OutputFile
-        val detektConfigurationFile: Provider<File> = extension.detektConfigurationFile
+@Inject
+constructor(private val extension: KotlinQAExtension) : DefaultTask() {
+    /**
+     * The output file (read only, must be configured in the [KotlinQAExtension].
+     */
+    @OutputFile
+    val detektConfigurationFile: Provider<File> = extension.detektConfigurationFile
 
-        /**
-         * Copies the default configuration from the classpath into the destination file.
-         */
-        @TaskAction
-        fun generateDetektConfigurationFile() {
-            val configuration =
-                Thread
-                    .currentThread()
-                    .contextClassLoader
-                    .getResource(DETEKT_CONFIG_FILE)
-                    ?.readText()
-                    ?: error(
-                        "Unable to read $DETEKT_CONFIG_FILE from the classpath, is this a bug in the Kotlin QA plugin?",
-                    )
-            val workDir = extension.workingDirectory.get()
-            if (workDir.mkdirs()) {
-                logger.debug("Created folder ${workDir.absolutePath}")
-            } else {
-                logger.debug("Folder ${workDir.absolutePath} was already existing")
-            }
-            detektConfigurationFile.get().writeText(configuration)
+    /**
+     * Copies the default configuration from the classpath into the destination file.
+     */
+    @TaskAction
+    fun generateDetektConfigurationFile() {
+        val configuration = Thread.currentThread().contextClassLoader
+            .getResource(DETEKT_CONFIG_FILE)
+            ?.readText()
+            ?: error("Unable to read $DETEKT_CONFIG_FILE from the classpath, is this a bug in the Kotlin QA plugin?")
+        val workDir = extension.workingDirectory.get()
+        if (workDir.mkdirs()) {
+            logger.debug("Created folder ${workDir.absolutePath}")
+        } else {
+            logger.debug("Folder ${workDir.absolutePath} was already existing")
         }
-
-        private companion object {
-            private const val DETEKT_CONFIG_FILE = "org/danilopianini/kotlinqa/detekt.yml"
-        }
+        detektConfigurationFile.get().writeText(configuration)
     }
+
+    private companion object {
+        private const val DETEKT_CONFIG_FILE = "org/danilopianini/kotlinqa/detekt.yml"
+    }
+}
--- src/main/kotlin/org/danilopianini/kotlinqa/KotlinQAExtension.kt
+++ src/main/kotlin/org/danilopianini/kotlinqa/KotlinQAExtension.kt
@@ -9,38 +9,28 @@ import org.gradle.kotlin.dsl.property
 /**
  * Gradle extension for the Kotlin QA Plugin.
  */
-open class KotlinQAExtension(
-    project: Project,
-) {
+open class KotlinQAExtension(project: Project) {
     /**
      * The folder where the Detekt configuration will be generated.
      */
-    val workingDirectory: Property<File> =
-        project.objects.property<File>().apply {
-            set(
-                project.layout.buildDirectory.asFile
-                    .map { File(it, "kotlinqa") },
-            )
-        }
+    val workingDirectory: Property<File> = project.objects.property<File>().apply {
+        set(project.layout.buildDirectory.asFile.map { File(it, "kotlinqa") })
+    }
 
     /**
      * The name of the generated Detekt configuration file.
      */
-    val detektConfigurationFileName: Property<String> =
-        project.objects.property<String>().apply {
-            set("detekt.yml")
-        }
+    val detektConfigurationFileName: Property<String> = project.objects.property<String>().apply {
+        set("detekt.yml")
+    }
 
     /**
      * The generated Detekt configuration file. Read only, use [workingDirectory] and [detektConfigurationFileName]
      * to change the value of this property.
      */
-    val detektConfigurationFile: Provider<File> =
-        workingDirectory.flatMap { workDir ->
-            detektConfigurationFileName.map { fileName ->
-                project.file("${workDir.absolutePath}${slash}$fileName")
-            }
-        }
+    val detektConfigurationFile: Provider<File> = workingDirectory.flatMap { workDir ->
+        detektConfigurationFileName.map { fileName -> project.file("${workDir.absolutePath}${slash}$fileName") }
+    }
 
     private companion object {
         private val slash: String = File.separator
