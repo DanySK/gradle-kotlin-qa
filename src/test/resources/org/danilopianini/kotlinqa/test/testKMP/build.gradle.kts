f2521e350a1b0385f222207847109b085adf10c4
Danilo Pianini <danilo.pianini@unibo.it>
2025-11-18 00:00:38 +0100

    test: update kotlin in the no-source test (#1246)
    * test: remove references to the version of kotlin-qa in the KMPP test
    
    * test: update kotlin in the no-source test

--- src/test/resources/org/danilopianini/kotlinqa/test/testKMP/build.gradle.kts
+++ src/test/resources/org/danilopianini/kotlinqa/test/testKMP/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0
 
 plugins {
-    alias(libs.plugins.kotlin.qa)
+    id("org.danilopianini.gradle-kotlin-qa")
     alias(libs.plugins.kotlin.multiplatform)
 }
 
