de16aeeca297158e76919ed44c9f3650b322cbfe
Danilo Pianini <danilo.pianini@unibo.it>
2026-01-18 00:42:27 +0100

    test: create a settings.gradle.kts with develocity for the base test (#1243)

---
+++ src/test/resources/org/danilopianini/kotlinqa/test/test0/settings.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("com.gradle.develocity") version "4.2.2"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+develocity {
+    buildScan {
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
+        uploadInBackground = !System.getenv("CI").toBoolean()
+    }
+}
